bde38e6e88dd16a128b4db60b60da15515c819cd
bharath <bharathbk2121@gmail.com>
2024-05-15 09:09:00 +0530

    Code for union find

---
+++ unionFind.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std ; 
+
+
+
+class UF { 
+private:
+  vector<int> id ;
+public:
+
+UF(int size){
+id.resize(size) ; 
+for(int i = 0 ; i< size ; i++){
+    id[i] = i  ; 
+  }
+
+ bool isConnected(int p , int q){
+    return id[p] == id[q] ; 
+  }
+
+    void union(int p, int q){
+      int val = id[p];
+      int val2 = id[q];
+      for(int i = 0 ; i < id.size() ; i++){
+        if(id[i]==val) id[i] = val2 ; 
+      }
+    }
+  }
+};
