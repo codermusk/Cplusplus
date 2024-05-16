30ec881e4dd69bb9444a89f8cfb3221564ac5210
bharath <bharathbk2121@gmail.com>
2024-05-16 09:03:38 +0530

    POTD

---
+++ a.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std ;
+#define ll long long 
+#define vi vector<int>
+#define ld long double
+#define pb push_back 
+#define REP(i,a,b) for(int i = a ; i < b ; i++)
+#define all(c) (c).begin(), (c).end()
+
+void solve(){
+
+}
+int main(){
+   #ifndef ONLINE_JUDGE 
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout); 
+   #endif 
+   int tc ; 
+   cin>> tc ; 
+   REP(i,0,tc)
+   solve(); 
+}
