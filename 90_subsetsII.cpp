bfa569c81028508c2302cec11584f4c0c51cf230
bharath <bharathbk2121@gmail.com>
2024-05-22 14:56:44 +0530

    Practising backtracking Sort the numbers inorder to skip the duplicate subset

--- 90_subsetsII.cpp
+++ 90_subsetsII.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    void backTrack(vector<int> &temp , vector<vector<int>> &sol , int start , vector<int> nums){
+    void backTrack(vector<int> &temp , vector<vector<int>> &sol , int start , vector<int>& nums){
         sol.push_back(temp);
         for(int i = start ; i< nums.size() ; i++){
-            if(i > start && nums[i-1] == nums[i]) continue ; 
+            if(i > start && nums[i] == nums[i-1]) continue ; 
             temp.push_back(nums[i]) ; 
             backTrack(temp, sol,i+1 , nums );
             temp.pop_back() ;
@@ -11,6 +11,7 @@ public:
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int> temp ; 
+        sort(nums.begin() , nums.end()) ;
         vector<vector<int>> sol ;
         backTrack(temp, sol, 0,nums);
         return sol ;
